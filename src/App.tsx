359c399c696c61ce5b4b76d964116d195dd0a80a
HSushynska <stoyanova.gv@gmail.com>
2025-03-25 18:16:14 +0100

    add lesson 06

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+// импорт компонента Lesson06 по умолчанию
+// при импорте по умолчанию можно использовать другое названия для компонента
+// import Task6 from './lessons/Lesson06/Lesson06';
+import Lesson06 from './lessons/Lesson06/Lesson06';
+
 function App() {
 
-return (
+  return (
     <>
-      First React+TS project
+      {/* <Task6 /> */}
+      <Lesson06 />
+      {/* <Homework06/> */}
     </>
   )
 }
 
-export default App;
+export default App
